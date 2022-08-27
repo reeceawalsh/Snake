4b3227da7f8fe2b44628fb761eee1b88ad92272d
reeceawalsh <reeceawalsh@hotmail.co.uk>
2022-08-27 09:51:28 +0100

    Setup files

---
+++ src/GamePanel.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+    GamePanel(){
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
